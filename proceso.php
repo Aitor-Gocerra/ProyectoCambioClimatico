66e53b3b11dcaf589328e1a15af31c315f7debbe
Aitor-Gocerra <aitorgomezcerrato.guadalupe@alumnado.fundacionloyola.net>
2025-09-22 17:19:01 +0200

    Update proceso.php

--- proceso.php
+++ proceso.php
@@ -1,5 +1,26 @@
 <?php
+    $_datos = [
+        "nombre" => $_POST["nombre"],
+        "apellidos" => $_POST["apellidos"],
+        "telefono" => $_POST["telefono"],
+        "direccion" => $_POST["direccion"],
+        "deportes" => $_POST["deporte"]
+    ];
 
+    print_r($_datos);
+
+    foreach ($_datos as $clave => $valor){
+        echo $clave . ": ";
+        if(is_array($valor)){
+            foreach ($valor as $data){
+            echo $data . " ";
+            }
+        }else{
+            echo $valor;
+        }
+        echo '<br>';
+
+    }
 
 
 ?>
