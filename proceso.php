bbbd2c8b90327576319df4e06958dde3e68fd374
Aitor-Gocerra <aitorgomezcerrato.guadalupe@alumnado.fundacionloyola.net>
2025-09-23 14:41:15 +0200

    Update proceso.php

--- proceso.php
+++ proceso.php
@@ -1,4 +1,9 @@
 <?php
+
+    // PROCESO 1 DANDO WARNINGS
+
+    echo '<h1> PROCESO 1 </h1>';
+
     $_datos = [
         "nombre" => $_POST["nombre"],
         "email" => $_POST["email"],
@@ -10,19 +15,43 @@
     ];
 
     print_r($_datos);
+    echo '<br>';
 
     foreach ($_datos as $clave => $valor){
-        echo $clave . ": ";
-        if(is_array($valor)){
-            foreach ($valor as $data){
-            echo $data . " ";
+        if(isset($valor)){
+            echo $clave . ": ";
+            if(is_array($valor)){
+                foreach ($valor as $data){
+                    echo $data . " ";
+                }
+            }else{
+                echo $valor;
             }
-        }else{
-            echo $valor;
-        }
-        echo '<br>';
+            echo '<br>';
+        }    
 
     }
 
+    // PROCESO 2 ALMACENANDO SOLO AQUELLOS DATOS QUE RECOJO DEL FORMULARIO
+
+    echo '<br>';
+    echo '<h1> PROCESO 2 </h1>';
+
+    if ($_POST) {
+        $datos = [];
+        
+        $claves = ["nombre", "email", "telefono", "password", "genero", "intereses", "condiciones"];
+        
+        foreach ($claves as $clave) {
+            
+            if (isset($_POST[$clave]) && $_POST[$clave] !== '') {
+                $datos[$clave] = $_POST[$clave];
+            }
+        }
+        
+        echo "Datos guardados: ";
+        echo '<br>';
+        print_r($datos);
+    }
 
 ?>
